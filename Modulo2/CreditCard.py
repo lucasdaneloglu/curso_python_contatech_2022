1db08d982b33a07326c50d0b97d4971743076570
DarioDiGulio <dariodigulio@gmail.com>
2022-04-09 09:56:02 -0300

    Ejercicios OOP I fixes

--- Modulo2/CreditCard.py
+++ Modulo2/CreditCard.py
@@ -1,18 +1,18 @@
 class CreditCard:
     def __init__(self, number: str, customer: str, buy_limit: float, available_limit: float):
-        self.number = number
-        self.customer = customer
-        self.buy_limit = buy_limit
-        self.available_limit = available_limit
+        self.__number = number
+        self.__customer = customer
+        self.__buy_limit = buy_limit
+        self.__available_limit = available_limit
 
     def __can_buy(self, amount: float) -> bool:
-        return amount <= self.available_limit
+        return amount <= self.__available_limit
 
     def buy(self, amount: float) -> bool:
         if self.__can_buy(amount):
-            self.available_limit -= amount
+            self.__available_limit -= amount
             return True
         return False
 
     def update_limit(self, new_limit: float) -> None:
-        self.available_limit = new_limit
+        self.__available_limit = new_limit
